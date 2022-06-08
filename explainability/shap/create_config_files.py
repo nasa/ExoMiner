a5313978589b7b7016898fc14bf57a3e1d48fa25
msaragoc <migueljorge.saragocamartinho@nasa.gov>
2022-06-08 14:54:51 -0700

    - SHAP: added configuration file config_create_run.yaml to handle parameters for the SHAP run.

--- explainability/shap/create_config_files.py
+++ explainability/shap/create_config_files.py
@@ -7,55 +7,26 @@ from pathlib import Path
 import itertools
 import yaml
 import numpy as np
+from datetime import datetime
 
 # local
 from utils.utils_dataio import is_yamlble
 
-config_dir = Path('/data5/tess_project/experiments/current_experiments/explainability/shap_1-18-2022')
+# load configuration for the explainability run
+path_to_yaml = Path('/Users/msaragoc/OneDrive - NASA/Projects/exoplanet_transit_classification/codebase/explainability/config_occlusion.yaml')
+with(open(path_to_yaml, 'r')) as file:
+    run_config = yaml.safe_load(file)
+
+# create experiment directory
+config_dir = Path(run_config['exp_dir'] / f'shap_{datetime.now().strftime("%m-%d-%Y_%H%M")}')
 config_dir.mkdir(exist_ok=True)
 runs_dir = config_dir / 'runs_configs'
 runs_dir.mkdir(exist_ok=True)
 
-# default configuration files; parameters shared by all configurations are already filled
-default_train_config_fp = Path(
-    '/data5/tess_project/experiments/current_experiments/explainability/shap_1-18-2022/default_config_train.yaml')
-default_pred_config_fp = Path(
-    '/data5/tess_project/experiments/current_experiments/explainability/shap_1-18-2022/default_config_predict.yaml')
-
-# branches' names (for model) to nicknames (for person)
-branches = {
-    'global_flux_view_fluxnorm': 'gf',
-    'local_flux_view_fluxnorm': 'lf',
-    'local_flux_oddeven_views': 'oe',
-    'global_centr_view_std_noclip': 'gc',
-    'local_centr_view_std_noclip': 'lc',
-    'local_weak_secondary_view_max_flux-wks_norm': 'wks',
-    'stellar': 's',
-    'dv+tce_fit': 'dvtf'
-}
-
-# mapping of branches to features in each branch
-branches_features = {
-    'global_flux_view_fluxnorm': ['global_flux_view_fluxnorm'],
-    'local_flux_view_fluxnorm': ['local_flux_view_fluxnorm', 'transit_depth_norm'],
-    'local_flux_oddeven_views': ['local_flux_odd_view_fluxnorm', 'local_flux_even_view_fluxnorm', 'odd_se_oot_norm',
-                                 'even_se_oot_norm'],
-    'global_centr_view_std_noclip': ['global_centr_view_std_noclip'],
-    'local_centr_view_std_noclip': ['local_centr_view_std_noclip', 'tce_fwm_stat_norm', 'tce_dikco_msky_norm',
-                                    'tce_dikco_msky_err_norm', 'tce_dicco_msky_norm', 'tce_dicco_msky_err_norm',
-                                    'mag_cat'],
-    'local_weak_secondary_view_max_flux-wks_norm': ['local_weak_secondary_view_max_flux-wks_norm', 'tce_maxmes_norm',
-                                                    'wst_depth_norm', 'tce_albedo_stat_norm', 'tce_ptemp_stat_norm'],
-    'stellar': ['tce_sdens_norm', 'tce_steff_norm', 'tce_smet_norm', 'tce_slogg_norm', 'tce_smass_norm',
-                'tce_sradius_norm'],
-    'dv+tce_fit': ['boot_fap_norm', 'tce_cap_stat_norm', 'tce_hap_stat_norm', 'tce_rb_tcount0n_norm', 'tce_prad_norm',
-                   'tce_period_norm']
-}
-
 # create combinations of branches
-n_branches = len(branches)
-branches_names = list(branches.keys())
-branches_nicknames = list(branches.values())
+n_branches = len(run_config['branches'])
+branches_names = list(run_config['branches'].keys())
+branches_nicknames = list(run_config['branches'].values())
 branches_range = np.arange(n_branches)
 
 runs = {}
@@ -66,7 +37,7 @@ for L in range(1, n_branches + 1):
         branches_run = [branches_names[idx] for idx in range(n_branches) if idx in subset]
         features_run = []
         for branch in branches_run:
-            features_run.extend(branches_features[branch])
+            features_run.extend(run_config['branches_features'][branch])
         runs['-'.join(np.array(branches_nicknames)[np.array(subset)])] = {'branches': branches_run,
                                                                           'features': features_run}
 
@@ -88,10 +59,10 @@ if config_runs_fp.is_file():
     config_runs_fp.unlink()
 # aa
 for run_name, run_data in runs.items():
-    with(open(default_train_config_fp, 'r')) as file:  # read default YAML configuration pred file
+    with(open(run_config['default_train_config_fp'], 'r')) as file:  # read default YAML configuration pred file
         train_config = yaml.safe_load(file)
 
-    with(open(default_pred_config_fp, 'r')) as file:  # read default YAML configuration pred file
+    with(open(run_config['default_pred_config_fp'], 'r')) as file:  # read default YAML configuration pred file
         pred_config = yaml.safe_load(file)
 
     # set experiment directory for training
