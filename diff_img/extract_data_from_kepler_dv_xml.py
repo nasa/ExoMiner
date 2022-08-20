5aa4b507bb2f1b0c5cfc35c94d95a642339fa95f
msaragoc <migueljorge.saragocamartinho@nasa.gov>
2022-08-19 17:20:53 -0700

    - Difference Imaging: created scripts get_statistics_kepler_diff_imgs.py and get_statistics_tess_diff_imgs.py to explore and analyze the difference image data from Kepler and TESS before further preprocessing.
    Signed-off-by: msaragoc <migueljorge.saragocamartinho@nasa.gov>

--- diff_img/extract_data_from_kepler_dv_xml.py
+++ diff_img/extract_data_from_kepler_dv_xml.py
@@ -24,21 +24,29 @@ if __name__ == '__main__':
     # create run directory
     run_dir.mkdir(exist_ok=True, parents=True)
 
+    data_dir = run_dir / 'data'
+    data_dir.mkdir(exist_ok=True)
+
     # load TCE table to get examples for which the image data are going to be extracted
     tce_tbl = pd.read_csv(tce_tbl_fp, usecols=['uid', 'label'])
     tce_tbl.set_index('uid', inplace=True)
     tces = tce_tbl
 
-    # creating plotting directory
+    # creatr plotting directory
     plot_dir = run_dir / 'plots'
     plot_dir.mkdir(exist_ok=True)
     plot_prob = 0.01
+
+    # create log directory
+    log_dir = run_dir / 'logs'
+    log_dir.mkdir(exist_ok=True)
 
     n_processes = 5
     pool = multiprocessing.Pool(processes=n_processes)
     n_jobs = 10
     tces_jobs = np.array_split(tces, n_jobs)
-    jobs = [(dv_xml_fp, tces_job, run_dir, plot_prob, job_i) for job_i, tces_job in enumerate(tces_jobs)]
+    jobs = [(dv_xml_fp, tces_job, data_dir, plot_dir, plot_prob, log_dir, job_i)
+            for job_i, tces_job in enumerate(tces_jobs)]
     async_results = [pool.apply_async(get_data_from_kepler_dv_xml_multiproc, job) for job in jobs]
     pool.close()
 
@@ -52,7 +60,7 @@ if __name__ == '__main__':
 
     # aggregating difference image data into a single numpy file
     data = {}
-    for data_fp in sorted([fp for fp in run_dir.iterdir() if 'keplerq1q17_dr25_diffimg_' in fp.name and
-                                                             fp.suffix == '.npy']):
+    for data_fp in sorted([fp for fp in data_dir.iterdir() if 'keplerq1q17_dr25_diffimg_' in fp.name and
+                                                              fp.suffix == '.npy']):
         data.update(np.load(data_fp, allow_pickle=True).item())
     np.save(run_dir / 'keplerq1q17_dr25_diffimg.npy', data)
