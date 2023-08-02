dc6895704bfcab78c34e91e5893d81dd51cb2ad1
Martinho, Miguel (ARC-TI)[UNIVERSITIES SPACE RESEARCH ASSN] <miguel.martinho@nasa.gov>
2023-08-02 09:42:48 -0700

    - Probing Multi-planet Systems: created compare_experiments.ipynb where I put Neha's code to compare different experiments' CDE losses across the training, validation and test sets. - Probing Multi-planet Systems: cleaned code in run_nnkcde_model.ipynb. - Probing Multi-planet Systems: created utils_cde_diagnostics.py where I placed the code related to plotting PIT and HPD values.

--- src_preprocessing/reduce-dataset.py
+++ src_preprocessing/reduce-dataset.py
@@ -5,24 +5,26 @@ import random
 total = 0
 p_keep = 25
 
-src_tfrec_dir = Path('/nobackup/cyates2/data/cvs/cv_merged_10-13-2022/tfrecords-normalized')
-dest_tfrec_dir = Path('/nobackup/cyates2/data/cvs/cv_merged_10-13-2022/tfrecords-reduced')
-dest_tfrec_dir.mkdir(exist_ok=True)
-for src_tfrec_fp in src_tfrec_dir.iterdir():
+src_tfrec_dir = Path('/Users/msaragoc/Projects/exoplanet_transit_classification/data/tfrecords/combined/cv_merged_10-13-2022/updt_diffimg_centr_2-8-2023/tfrecords/eval/')
+# dest_tfrec_dir = Path('/nobackup/cyates2/data/cvs/cv_merged_10-13-2022/tfrecords-reduced')
+# dest_tfrec_dir.mkdir(exist_ok=True)
+for src_tfrec_fp in [fp for fp in src_tfrec_dir.iterdir() if fp.name.startswith('train-TESS')]:
     if "train" not in src_tfrec_fp.name:
         continue
-    dest_tfrec_fp = dest_tfrec_dir / src_tfrec_fp.name
+    # dest_tfrec_fp = dest_tfrec_dir / src_tfrec_fp.name
 
-    with tf.io.TFRecordWriter(str(dest_tfrec_fp)) as writer:
-        print(dest_tfrec_fp)
+    # with tf.io.TFRecordWriter(str(dest_tfrec_fp)) as writer:
+    #     print(dest_tfrec_fp)
 
-        tfrecord_dataset = tf.data.TFRecordDataset(str(src_tfrec_fp))
-        for string_record in tfrecord_dataset.as_numpy_iterator():
-            example = tf.train.Example()
-            example.ParseFromString(string_record)
-            if random.random() < p_keep/100:
-                writer.write(example.SerializeToString())
-                total += 1
+    tfrecord_dataset = tf.data.TFRecordDataset(str(src_tfrec_fp))
+    for string_record in tfrecord_dataset.as_numpy_iterator():
+        example = tf.train.Example()
+        example.ParseFromString(string_record)
+        aa
+        # if random.random() < p_keep/100:
+        #     writer.write(example.SerializeToString())
+        #     total += 1
+
 print(total)
 
 
