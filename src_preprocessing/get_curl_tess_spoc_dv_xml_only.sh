31589da294c38e8d621b06bbb0e2b0757b0cbdbf
Miguel Martinho <miguel.martinho@nasa.gov>
2024-07-02 16:36:14 -0700

    - Updated ExoMiner architecture image. - src: in train_model.py, results are now saved as res_train.npy instead of res_eval.npy like the evaluate model fn. - TESS SPOC FFI: fixed issue in download_tess_spoc_ffi_lc_files.sh (missing $). - TESS SPOC FFI: cleaned extract_tce_data_from_dv_xml.py - Preprocessing: filename of dv.sh file in get_curl_tess_spoc_dv_xml_only.sh is now based on index and not hardcoded.

--- src_preprocessing/get_curl_tess_spoc_dv_xml_only.sh
+++ src_preprocessing/get_curl_tess_spoc_dv_xml_only.sh
@@ -1,15 +1,16 @@
 # Create new sh script with curl commands only for xml files.
 
-TARGET_SH_DIR=/data5/tess_project/Data/tess_spoc_ffi_data/dv/target_sh/
-DEST_DIR=/data5/tess_project/Data/tess_spoc_ffi_data/dv/target_sh_xml_only/
+TARGET_SH_DIR=/Users/msaragoc/Downloads/tess_spoc_ffi_fits_sh/dv
+DEST_DIR=/Users/msaragoc/Downloads/tess_spoc_ffi_fits_sh/dv_xml
 
 for sector_shfile in "$TARGET_SH_DIR"/*dv.sh
 do
-   FILENAME=${sector_shfile:10}
+   FILENAME_INDEX=$(echo $sector_shfile | grep -bo "hlsp_*" | grep -oe "[0-9]*")
+   FILENAME=${sector_shfile:$FILENAME_INDEX}
    echo "$FILENAME"
    grep xml "$sector_shfile" > $DEST_DIR/"$FILENAME"
 done
 
-# set permissions and group
+# set permissions and group when in ranokau
 chgrp -R ar-gg-ti-tess-dsg $DEST_DIR
 chmod -R 770 $DEST_DIR
