88258989c5cb0b0c92bb2c36a6e775a829d23abd
Josue Ochoa <josue.j.ochoa@nasa.gov>
2024-09-16 00:08:39 -0700

    Updated pipeline to use target, sector_run pairs

--- transit_detection/build_dataset.py
+++ transit_detection/build_dataset.py
@@ -12,20 +12,22 @@ import warnings
 import tensorflow as tf
 
 # local
-from transit_detection.utils_flux_processing import extract_flux_windows_for_tce
+from transit_detection.utils_flux_processing import extract_flux_windows_for_tce, build_transit_mask_for_lightcurve
 from transit_detection.utils_difference_img_processing import extract_diff_img_data_from_window
 from transit_detection.utils_build_dataset import serialize_set_examples_for_tce, write_data_to_auxiliary_tbl
 
+from src_preprocessing.lc_preprocessing.detrend_timeseries import detrend_flux_using_sg_filter
 #%%
 
 # set dataset directory
-data_dir = Path('/Users/msaragoc/Downloads/transit_detection/data/test_create_dataset_5-31-2024_1017')
+data_dir = Path('/Users/jochoa4/Downloads/transit_detection/data/test_create_dataset_5-31-2024_1017')
 # set light curve data directory
-lc_dir = Path('/Users/msaragoc/Downloads/transit_detection/data/lc_data/')
+lc_dir = Path('/Users/jochoa4/Downloads/transit_detection/data/lc_data/')
 # set target pixel file data directory
-tpf_dir = Path('/Users/msaragoc/Downloads/transit_detection/data/tpf_data/')
+tpf_dir = Path('/Users/jochoa4/Downloads/transit_detection/data/tpf_data/')
 # TCE table
-tce_tbl = pd.read_csv('/Users/msaragoc/Projects/exoplanet_transit_classification/data/ephemeris_tables/tess/DV_SPOC_mat_files/preprocessing_tce_tables/09-25-2023_1608/tess_2min_tces_dv_s1-s68_all_msectors_11-29-2023_2157_newlabels.csv')
+tce_tbl = pd.read_csv('/Users/jochoa4/Projects/exoplanet_transit_classification/ephemeris_tables/preprocessing_tce_tables/tess_2min_tces_dv_s1-s68_all_msectors_11-29-2023_2157_newlabels_nebs_npcs_bds_ebsntps_to_unks.csv')
+tce_tbl = tce_tbl.loc[tce_tbl['label'].isin(['EB','KP','CP','NTP','NEB','NPC'])] #filter for relevant labels
 tce_tbl.rename(columns={'label': 'disposition', 'label_source': 'disposition_source'}, inplace=True)
 n_durations_window = 5  # number of transit durations in each extracted window
 frac_valid_cadences_in_window_thr = 0.85
@@ -65,16 +67,47 @@ print(tce_tbl[['uid', 'disposition']])
 rng = np.random.default_rng(seed=rnd_seed)
 
 data_to_tfrec = []
-for tce_i, tce_data in tce_tbl.iterrows():  # iterate over each TCE
+
+# for target in tce_tbl['target_id'].unique(): # for each target in tce_tbl
+#     print(f'Processing target: {target}')
+
+#     target_tces = []
+#     for tce_i, tce_data in tce_tbl[tce_tbl['target_id'] == target].iterrows(): # for all tces in target
+#         tce_uid = tce_data['uid']
+
+#         target_tces.append({
+#             "tce_time0bk" : tce_data['tce_time0bk'],
+#             "tce_period" : tce_data['tce_period'],
+#             "tce_duration" : tce_data['tce_duration']
+#         })
+#     print(target_tces)
+
+#     for sector_run, sector_run_df in tce_tbl[tce_tbl['target_id'] == target].groupby('sector_run'): # for each target, sector pair
+    
+#         for tce_i, tce_data in sector_run_df.iterrows(): # for each tce in target, sector pair
+#             #regular pipeline
+            
+
+for (target, sector_run), sector_run_data in tce_tbl.groupby(['target_id','sector_run']):
+
+    target_tce_data = []
+
+    for tce_i, tce_data in tce_tbl[tce_tbl['target_id'] == target].iterrows(): # for all tces associated with target
+        target_tce_data.append({
+            "tce_time0bk" : tce_data['tce_time0bk'],
+            "tce_period" : tce_data['tce_period'],
+            "tce_duration" : tce_data['tce_duration']
+        })
+
+    transit_mask = []
+
+    # print(f'Extracting and processing data for target {sector_run_data["target_id"][0]}...')
 
     data_for_tce = {
-        'tce_uid': tce_data['uid'],
-        'tce_info': tce_data,
-        'sectors': [],
+    'tce_uid': sector_run_data['uid'],
+    'tce_info': tce_data,
+    'sectors': [],
     }
-
-    print(f'Extracting and processing data for TCE {tce_data["uid"]}...')
-
     # create directory for TCE plots
     plot_dir_tce = plot_dir / f'tce_{tce_data["uid"]}'
     plot_dir_tce.mkdir(exist_ok=True, parents=True)
@@ -97,20 +130,20 @@ for tce_i, tce_data in tce_tbl.iterrows():  # iterate over each TCE
     tce_duration = tce_data['tce_duration']
 
     # find light curve data for target
-    search_lc_res = lk.search_lightcurve(target=f"tic{tic_id}", mission='TESS', author=('TESS-SPOC', 'SPOC'),
-                                         exptime=120, cadence='long', sector=sector_run_arr)
+    search_lc_res = lk.search_lightcurve(target=f"tic{target}", mission='TESS', author=('TESS-SPOC', 'SPOC'),
+                                        exptime=120, cadence='long', sector=sector_run_arr) #get ALL sectors
 
     found_sectors = [int(el.split(' ')[-1]) for el in search_lc_res.mission]
     print(f'Found {len(found_sectors)} sectors with light curve data for TIC {tic_id}.')
 
-    for sector_i, sector in enumerate(found_sectors):  # iterate over sectors
+    for sector_i, sector in enumerate(found_sectors):  # for given sector_run
 
         data_for_tce_sector = {
             'sector': sector,
             'transit_examples': {'flux': [], 't': [], 'it_img': [], 'oot_img': [], 'diff_img': [], 'snr_img': [],
-                                 'target_img': [], 'target_pos': []},
+                                'target_img': [], 'target_pos': []},
             'not_transit_examples': {'flux': [], 't': [], 'it_img': [], 'oot_img': [], 'diff_img': [], 'snr_img': [],
-                                     'target_img': [], 'target_pos': []},
+                                    'target_img': [], 'target_pos': []},
         }
 
         # if len(search_lc_res) == 0:
@@ -122,39 +155,38 @@ for tce_i, tce_data in tce_tbl.iterrows():  # iterate over each TCE
 
         # download light curve fits file(s)
         lcf = search_lc_res[sector_i].download(download_dir=str(lc_dir), quality_bitmask='default',
-                                                   flux_column='pdcsap_flux')
+                                                flux_column='pdcsap_flux')
 
-        # # find light curve data for target
-        # search_lc_res = lk.search_lightcurve(target=f"tic{tic_id}", mission='TESS', author=('TESS-SPOC', 'SPOC'),
-        #                                      exptime=120, cadence='long', sector=sector_run_arr)
-        # print(search_lc_res)
-        # # download light curve fits file(s)
-        # lcf = search_lc_res.download_all(download_dir=str(lc_dir), quality_bitmask='default', flux_column='pdcsap_flux')
-        # lcf = lcf.stitch(lambda x: lk.LightCurve(time=x.time, flux=np.array(x.flux)))
-
-        # with fits.open(lcf.filename, mode="readonly") as hdulist:
-        #
-        #     time = hdulist[1].data['TIME']
-        #     pdcsap_flux = hdulist[1].data['PDCSAP_FLUX']
-        #     dq_flags = hdulist[1].data['QUALITY']
+        # 1)get all in transit cadence for all TCEs in target, sector pair
+        # 2)create mask of intransit cadences
+        # 3)detrend flux using mask
 
         time, flux = lcf.time.value, np.array(lcf.flux.value)
 
+        in_transit_mask = build_transit_mask_for_lightcurve(time=time, tce_list=target_tce_data)
+
+        time, flux = detrend_flux_using_sg_filter(lc=lcf, mask_in_transit=in_transit_mask, win_len=1.2, sigma=5,
+                                                    max_poly_order=6, penalty_weight=1, break_tolerance=5)
+
+
+    for tce_i, tce_data in sector_run_data.iterrows(): # iterate over each TCE in target, sector_run pair
+        #extract flux time series for each tce in tce_target_sector_run
+
         # extract flux time series windows
         (resampled_flux_it_windows_arr, resampled_flux_oot_windows_arr, midtransit_points_windows_arr,
-         midoot_points_windows_arr) = extract_flux_windows_for_tce(time, flux, tce_time0bk, period_days, tce_duration,
-                                                                   n_durations_window, gap_width,
-                                                                   buffer_time, frac_valid_cadences_in_window_thr,
-                                                                   frac_valid_cadences_it_thr,
-                                                                   resampled_num_points, tce_data['uid'], rng,
-                                                                   plot_dir_tce_sector)
+        midoot_points_windows_arr) = extract_flux_windows_for_tce(time, flux, tce_time0bk, period_days, tce_duration,
+                                                                n_durations_window, gap_width,
+                                                                buffer_time, frac_valid_cadences_in_window_thr,
+                                                                frac_valid_cadences_it_thr,
+                                                                resampled_num_points, tce_data['uid'], rng,
+                                                                plot_dir_tce_sector)
 
         plot_dir_tce_diff_img_data = plot_dir_tce_sector / 'diff_img_data'
         plot_dir_tce_diff_img_data.mkdir(exist_ok=True, parents=True)
 
         # find target pixel data for target
         search_result = lk.search_targetpixelfile(target=f"tic{tic_id}", mission='TESS', author=('TESS-SPOC', 'SPOC'),
-                                                  exptime=120, cadence="long", sector=sector)
+                                                exptime=120, cadence="long", sector=sector)
         # download target pixel file
         tpf = search_result[0].download(download_dir=str(tpf_dir), quality_bitmask='default')
 
@@ -170,13 +202,13 @@ for tce_i, tce_data in tce_tbl.iterrows():  # iterate over each TCE
             try:
                 diff_img_processed, oot_img_processed, snr_img_processed, target_img, target_pos_col, target_pos_row = (
                     extract_diff_img_data_from_window(tpf.path, midtransit_point_window, tce_duration, buffer_time,
-                                                      tce_data['ra'], tce_data['dec'], size_img, f_size,
-                                                      sector, center_target, tce_data['uid'],
-                                                      plot_dir=plot_dir_tce_transit_diff_img_data))
+                                                    tce_data['ra'], tce_data['dec'], size_img, f_size,
+                                                    sector, center_target, tce_data['uid'],
+                                                    plot_dir=plot_dir_tce_transit_diff_img_data))
             except ValueError as error:
                 warnings.warn(error.args[0])
                 warnings.warn(f'no difference image data computed for midtransit point {midtransit_point_window}, '
-                              f'skipping this window')
+                            f'skipping this window')
                 excl_idxs_it_ts.append(window_i)
                 continue
 
@@ -188,7 +220,7 @@ for tce_i, tce_data in tce_tbl.iterrows():  # iterate over each TCE
 
         # compute oot transit difference image data
         oot_diff_img_data = {feature_name: []
-                             for feature_name in ['diff_img', 'oot_img', 'snr_img', 'target_img', 'target_pos']}
+                            for feature_name in ['diff_img', 'oot_img', 'snr_img', 'target_img', 'target_pos']}
         plot_dir_tce_oottransit_diff_img_data = plot_dir_tce_diff_img_data / 'oot_transit_imgs'
         plot_dir_tce_oottransit_diff_img_data.mkdir(exist_ok=True, parents=True)
         excl_idxs_oot_ts = []
@@ -197,13 +229,13 @@ for tce_i, tce_data in tce_tbl.iterrows():  # iterate over each TCE
             try:
                 diff_img_processed, oot_img_processed, snr_img_processed, target_img, target_pos_col, target_pos_row = (
                     extract_diff_img_data_from_window(tpf.path, midoot_point_window, tce_duration, buffer_time,
-                                                      tce_data['ra'], tce_data['dec'], size_img, f_size,
-                                                      sector, center_target, tce_data['uid'],
-                                                      plot_dir=plot_dir_tce_oottransit_diff_img_data))
+                                                    tce_data['ra'], tce_data['dec'], size_img, f_size,
+                                                    sector, center_target, tce_data['uid'],
+                                                    plot_dir=plot_dir_tce_oottransit_diff_img_data))
             except ValueError as error:
                 warnings.warn(error.args[0])
                 warnings.warn(f'no difference image data computed for out-of-transit point {midoot_point_window}, '
-                              f'skipping this window')
+                            f'skipping this window')
                 excl_idxs_oot_ts.append(window_i)
                 continue
 
