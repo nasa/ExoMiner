da01c28f34a19cd70235d041c45645eebf8b31d9
Josue Ochoa <josue.j.ochoa@nasa.gov>
2024-09-30 18:14:38 -0700

    Updated return object structure

--- transit_detection/utils_local_fits_processing.py
+++ transit_detection/utils_local_fits_processing.py
@@ -15,12 +15,12 @@ def search_and_read_tess_lightcurve(target, sectors, data_dir):
 
     sector_paths = [Path(f"{data_dir}/sector_{sector}") for sector in sectors] if sectors else []
     found_sectors = []
-    light_curve_files = dict.fromkeys(sectors, None)
+    light_curve_files = []
     for sector, sector_path in zip(sectors, sector_paths):
         try:
             fits_file_path = list(sector_path.rglob(f"*{f'{target}'.zfill(16)}*lc.fits"))[0] #should only be 1 instance
             lcf = lk.read(fits_file_path)
-            light_curve_files.update({sector: lcf})
+            light_curve_files.append(lcf)
             found_sectors.append(sector)
         except:
             print(f"Error finding file {sector_path}.")
@@ -40,12 +40,12 @@ def search_and_read_tess_targetpixelfile(target, sectors, data_dir):
 
     sector_paths = [Path(f"{data_dir}/sector_{sector}") for sector in sectors] if sectors else []
     found_sectors = []
-    target_pixel_files = dict.fromkeys(sectors, None)
+    target_pixel_files = []
     for sector, sector_path in zip(sectors, sector_paths):
         try:
             fits_file_path = list(sector_path.rglob(f"*{f'{target}'.zfill(16)}*tp.fits"))[0] #should only be 1 instance
             tpf = lk.read(fits_file_path)
-            target_pixel_files.update({sector: tpf})
+            target_pixel_files.append(tpf)
             found_sectors.append(sector)
         except:
             print(f"Error finding file {sector_path}.")
