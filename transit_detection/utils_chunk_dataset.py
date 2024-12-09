23c065022609d8c79ef06b20241ddbac3608b367
Josue Ochoa <josue.j.ochoa@nasa.gov>
2024-12-09 12:45:53 -0800

    Corrected chunk name logic

--- transit_detection/utils_chunk_dataset.py
+++ transit_detection/utils_chunk_dataset.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 def build_chunk_mask(chunks_to_process, chunked_dataset_dir):
     """
     Builds mask to exclude chunks that have been successfully processed for an iteration of dataset building
@@ -5,13 +7,17 @@ def build_chunk_mask(chunks_to_process, chunked_dataset_dir):
 
     chunk_mask = [0] * len(chunks_to_process)
 
+    if not any(chunked_dataset_dir.iterdir()):
+        print('Directory is empty')
+        return chunk_mask
+
     shard_prefix = 'test_shard_0001-'
     aux_tbl_prefix = 'data_tbl_chunk_'
     
     for chunk_i, chunk in enumerate(chunks_to_process):
         try:
-            chunk_shard_path = list(chunked_dataset_dir.rglob(f"*{shard_prefix}{str(chunk).zfill(4)}"))[0]
-            chunk_aux_tbl_path = list(chunked_dataset_dir.rglob(f"*{aux_tbl_prefix}{str(chunk).zfill(4)}.csv"))[0]
+            chunk_shard_path = list(chunked_dataset_dir.rglob(f"*{shard_prefix}{str(chunk_i).zfill(4)}"))[0]
+            chunk_aux_tbl_path = list(chunked_dataset_dir.rglob(f"*{aux_tbl_prefix}{str(chunk_i).zfill(4)}.csv"))[0]
 
             if chunk_shard_path.exists() and chunk_aux_tbl_path.exists():
                 chunk_mask[chunk_i] = 1
