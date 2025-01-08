fc5cb26d59046d670a6d01cfade9f828d3c768be
Josue Jacobo Ochoa <jochoa4@pfe27.nas.nasa.gov>
2025-01-08 15:58:12 -0800

    Fixed chunk mask offset

--- transit_detection/utils_chunk_dataset.py
+++ transit_detection/utils_chunk_dataset.py
@@ -16,11 +16,11 @@ def build_chunk_mask(chunks_to_process, chunked_dataset_dir):
     
     for chunk_i, chunk in enumerate(chunks_to_process):
         try:
-            chunk_shard_path = list(chunked_dataset_dir.rglob(f"*{shard_prefix}{str(chunk_i).zfill(4)}"))[0]
-            chunk_aux_tbl_path = list(chunked_dataset_dir.rglob(f"*{aux_tbl_prefix}{str(chunk_i).zfill(4)}.csv"))[0]
+            chunk_shard_path = list(chunked_dataset_dir.rglob(f"*{shard_prefix}{str(chunk_i + 1).zfill(4)}"))[0]
+            chunk_aux_tbl_path = list(chunked_dataset_dir.rglob(f"*{aux_tbl_prefix}{str(chunk_i + 1).zfill(4)}.csv"))[0]
 
             if chunk_shard_path.exists() and chunk_aux_tbl_path.exists():
-                chunk_mask[chunk_i] = 1
+                chunk_mask[chunk_i + 1] = 1
             
         except:
             #chunk not found
