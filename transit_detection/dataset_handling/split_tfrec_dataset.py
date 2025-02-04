95bddd272419c81b8794ee3396ddce7e7b99cc4f
Josue Ochoa <josue.j.ochoa@nasa.gov>
2025-02-03 16:13:32 -0800

    Parallelized split using individual shards

--- transit_detection/dataset_handling/split_tfrec_dataset.py
+++ transit_detection/dataset_handling/split_tfrec_dataset.py
@@ -10,83 +10,60 @@ import numpy as np
 import pandas as pd
 from collections import defaultdict
 
+# TODO: Add multiple shards for train/test/val
+# can parallelize on multiple shards
+import multiprocessing
+from functools import partial
+from copy import deepcopy
+
 # local
 from src_preprocessing.tf_util import example_util
 
-# function to process a single shard for a given set of tce uids
-def compute_and_write_example_stats(all_set_feature_img_pixels_dict, dest_stats_dir):
+def process_shard(input_shard_num, input_dir, output_dir, train_targets, val_targets, test_targets):
 
-    for feature_img in all_set_feature_img_pixels_dict.keys():
-        median = all_set_feature_img_pixels_dict[feature_img].median()
-        mean = all_set_feature_img_pixels_dict[feature_img].mean()
-        std = all_set_feature_img_pixels_dict[feature_img].std()
+    input_shard_fp = input_dir / f'test_shard_0001-{input_shard_num}'
 
-        stats = {
-            'mean' : mean,
-            'std' : std,
-            'median' : median,
-            'total_pixels' : len(all_set_feature_img_pixels_dict[feature_img]),
-        }
+    train_tfrec_fp = output_dir / 'train_set'
+    val_tfrec_fp = output_dir / 'val_set'
+    test_tfrec_fp = output_dir / 'test_set'
 
-        np.save(dest_stats_dir / f"{feature_img}_stats.npy", stats)
-        print(f"Statistics for {feature_img} saved in a single .npy file")
-
-def process_aux_tbl(src_aux_tbl_fp, dest_aux_tbl_fp, train_targets, val_targets, test_targets, set_names):
-    aux_tbl = pd.read_csv(src_aux_tbl_fp)
-
-    aux_tbl.loc[aux_tbl['target_id'].isin(train_targets), 'tfrec_fp'] = set_names['train_set']
-    aux_tbl.loc[aux_tbl['target_id'].isin(val_targets), 'tfrec_fp'] = set_names['val_set']
-    aux_tbl.loc[aux_tbl['target_id'].isin(test_targets), 'tfrec_fp'] = set_names['test_set']
-
-    invalid_rows = aux_tbl[~aux_tbl['tfrec_fp'].isin([set_names['train_set'], set_names['val_set'], set_names['test_set']])]
-    num_invalid_rows = invalid_rows.shape[0]
-
-    if num_invalid_rows > 0:
-        print(f'ERROR: Number of invalid rows: {num_invalid_rows}')
-
-    aux_tbl.to_csv(dest_aux_tbl_fp, index=False)
-
-# function to process a split and write the output
-def process_split(target_ids, src_tfrec_fp, dest_tfrec_fp, dest_stats_dir=None):
-
-    all_set_feature_img_pixels = {
-        'diff_img' : np.array([]),
-        'oot_img' : np.array([]),
-        'snr_img' : np.array([]),
+    writers = {
+        "train" : tf.io.TFRecordWriter(train_tfrec_fp / f"train_shard_0001-{input_shard_num}"),
+        "val" : tf.io.TFRecordWriter(val_tfrec_fp / f"val_shard_0001-{input_shard_num}"),
+        "test" : tf.io.TFRecordWriter(test_tfrec_fp / f"test_shard_0001-{input_shard_num}")
     }
 
-    with tf.io.TFRecordWriter(str(dest_tfrec_fp)) as writer:
-        src_tfrec_dataset = tf.data.TFRecordDataset(str(src_tfrec_fp))
+    for str_record in tf.data.TFRecordDataset(input_shard_fp):
+        example = tf.train.Example()
+        example.ParseFromString(str_record)
 
-        for str_record in src_tfrec_dataset:
-            # load example
-            example = tf.train.Example()
-            example.ParseFromString(str_record)
+        target_id = example.features.feature['target_id'].bytes_list.value[0].decode('utf-8')
 
-            target_id = example.features.feature['target_id'].bytes_list.value[0].decode('utf-8')
-
-            if target_id in target_ids:
-                if dest_stats_dir: #compute stats for set
-                    # get pixel values for diff, oot, and snr imgs
-                    for feature_img in all_set_feature_img_pixels.keys():
-                        # load img feature
-                        feature_img_pixels = tf.reshape(tf.io.parse_tensor(example.features.feature[feature_img].bytes_list.value[0], tf.float32),
-                                                        [-1]).numpy() # flattened pixels in feature img
-                        all_set_feature_img_pixels[feature_img].extend(feature_img_pixels)
-
-                # write example to dest_tfrec_fp
-                writer.write(example.SerializeToString())
-
-        if dest_stats_dir: # write example stats to dest_stats_dir
-            compute_and_write_example_stats(all_set_feature_img_pixels, dest_stats_dir)
+        if target_id in train_targets:
+            split = "train"
+        elif target_id in val_targets:
+            split = "val"
+        elif target_id in test_targets:
+            split = "test"
+        else:
+            raise ValueError(f"Target ID {target_id} not found in any set")
         
+        writers[split].write(str_record.numpy())
+    
+    for writer in writers.values():
+        writer.close()
+
+    print(f"Processed shard: {input_shard_fp}")
+
+    return f"Completed: {input_shard_fp}"
+
 
 if __name__ == "__main__":
     # load tce tbl
     tce_tbl = pd.read_csv('/nobackup/jochoa4/work_dir/data/tables/tess_2min_tces_dv_s1-s68_all_msectors_11-29-2023_2157_newlabels_nebs_npcs_bds_ebsntps_to_unks.csv')
     tce_tbl = tce_tbl.loc[tce_tbl['label'].isin(['EB','KP','CP','NTP','NEB','NPC'])] #filter for relevant labels
 
-    # src directory for merged tfrecord shard
+    # src directory for raw tfrecord shards
     src_tfrec_dir = Path("/nobackup/jochoa4/work_dir/data/datasets/TESS_exoplanet_dataset_11-25-2024")
 
     # src fp for auxillary table
@@ -95,18 +72,14 @@ if __name__ == "__main__":
     # destination directory for tfrecord shard splits
     dest_tfrec_dir = Path("/nobackup/jochoa4/work_dir/data/datasets/TESS_exoplanet_dataset_11-25-2024_split")
 
-    # destination directory for computed training stats
-    dest_stats_dir = Path("/nobackup/jochoa4/work_dir/data/stats/TESS_exoplanet_dataset_11-25-2024_stats")
-
-    #destination fp for auxillary tbl based on tf record shard splits
-    dest_aux_tbl_fp = Path("/nobackup/jochoa4/work_dir/data/datasets/TESS_exoplanet_dataset_11-25-2024_split")
-
+    # destination directory for logging
+    dest_log_dir = Path("/nobackup/jochoa4/work_dir/data/logging") #TODO: revisit and update dest_log_dir
 
     # set random seed
     np.random.seed(42) 
 
     # get unique targets from tce_tbl
-    unique_targets = tce_tbl['target_id'].unique()
+    unique_targets = tce_tbl['target_id'].unique() # TODO: update with targets found in auxillary table
 
     # shuffle targets
     shuffled_targets = np.random.permutation(unique_targets) 
@@ -132,33 +105,58 @@ if __name__ == "__main__":
     print(f'Using {len(val_targets)} targets in validation set')
     print(f'Using {len(test_targets)} targets in test set')
 
-    set_names = {
-        'train_set' : 'train_set',
-        'val_set' : 'val_set',
-        'test_set' : 'test_set'
-    }
+    # Save the dataset targets to a npy file
+    dataset_targets = {}
+    dataset_targets['train_set'] = train_targets
+    dataset_targets['val_set'] = val_targets
+    dataset_targets['test_set'] = test_targets
 
-    train_tfrec_fp = dest_tfrec_dir / set_names['train_set']
-    val_tfrec_fp = dest_tfrec_dir / set_names['val_set']
-    test_tfrec_fp = dest_tfrec_dir / set_names['test_set']
+    np.save('dataset_targets.npy', dataset_targets)
 
-    # Build training set
-    process_split(train_targets, src_tfrec_dir, train_tfrec_fp, dest_stats_dir)
+    # # define logging??
 
-    # Build validation set
-    process_split(val_targets, src_tfrec_dir, val_tfrec_fp)
-
-    # Build test set
-    process_split(test_targets, src_tfrec_dir, test_tfrec_fp)
-
-    # Create new auxillary table with mappings based on set
-    process_aux_tbl(src_aux_tbl_fp, dest_aux_tbl_fp, train_targets, val_targets, test_targets, set_names)
+    # job_log_dir = log_dir / 'job_logs'
+    # job_log_dir.mkdir(parents=True, exist_ok=True)
 
 
+    #     # log chunk validation
+    # logger = logging.getLogger(f"chunk_validation_logger")
+    # logger.setLevel(logging.INFO)
+
+    # log_path = Path(log_dir) / f"chunk_validation.log"
+    # file_handler = logging.FileHandler(log_path)
+    # logger_formatter = logging.Formatter('%(asctime)s - %(levelname)s- %(message)s')
+    # file_handler.setFormatter(logger_formatter)
+    # logger.addHandler(file_handler)
+
+    # logger.info(f'Processing a total of {len(chunked_jobs)} chunks of size {job_chunk_size}')
+    # print(f'Processing a total of {len(chunked_jobs)} chunks of size {job_chunk_size}')
 
 
+    # logger.info(f'Skipping processing for {sum(processed_chunk_mask)} chunks that have already been processed.')
+
+
+
+    # define partial func to predefine default values for directories and target sets
+    partial_func = partial(process_shard,
+                                # def input_shard_num per job
+                                input_dir=src_tfrec_dir,
+                                output_dir=dest_tfrec_dir,
+                                train_targets=train_targets,
+                                val_targets=val_targets,
+                                test_targets=test_targets
+                                )
+
+    # using N-1 = 127
+    pool = multiprocessing.Pool(processes=127)
+
+    jobs = [shard_num for shard_num in range(1, 8612)] # chunk 1 to chunk 8611
+
+    for shard_num in jobs:
+        pool.apply_async(partial_func, args=shard_num)
+
+    pool.close()
+    pool.join()
     
-    
-
-
+    print('Finished processing train val test split')
 
