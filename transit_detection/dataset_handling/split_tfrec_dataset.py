54c6f4ffb40a41f20d11b6f97c7cc6e576d2483f
Josue Jacobo Ochoa <jochoa4@pfe27.nas.nasa.gov>
2025-02-04 14:20:37 -0800

    Updated split with proper filenames

--- transit_detection/dataset_handling/split_tfrec_dataset.py
+++ transit_detection/dataset_handling/split_tfrec_dataset.py
@@ -2,106 +2,152 @@
 Split tfrec dataset into training, validation, and test sets
 """
 
-
 # 3rd party
 import tensorflow as tf
 from pathlib import Path
 import numpy as np
 import pandas as pd
 from collections import defaultdict
+import logging
 
 import multiprocessing
 from functools import partial
 from copy import deepcopy
+import time
 
 # local
 from src_preprocessing.tf_util import example_util
 from src_preprocessing.lc_preprocessing.utils_manipulate_tfrecords import create_table_with_tfrecord_examples
 
+
 def process_shard(input_shard_num, input_dir, output_dir, train_targets, val_targets, test_targets):
+    try:
+        input_shard_num = str(input_shard_num).zfill(4)
 
-    input_shard_fp = input_dir / f'test_shard_0001-{input_shard_num}'
+        input_shard_fp = input_dir / f'test_shard_0001-{input_shard_num}'
 
-    train_tfrec_fp = output_dir / 'train_tfrecords' / f"train_shard_{input_shard_num}-8611"
-    val_tfrec_fp = output_dir / 'val_tfrecords' / f"val_shard_{input_shard_num}-8611"
-    test_tfrec_fp = output_dir / 'test_tfrecords' / f"test_shard_{input_shard_num}-8611"
+        train_tfrec_dir = output_dir / 'train_tfrecords'
+        train_tfrec_dir.mkdir(parents=True, exist_ok=True)
+        val_tfrec_dir = output_dir / 'val_tfrecords'
+        val_tfrec_dir.mkdir(parents=True, exist_ok=True)
+        test_tfrec_dir = output_dir / 'test_tfrecords'
+        test_tfrec_dir.mkdir(parents=True, exist_ok=True)
 
-    train_count = 0
-    val_count = 0
-    test_count = 0
+        train_tfrec_fp = train_tfrec_dir / f"train_shard_{input_shard_num}-8611"
+        val_tfrec_fp = val_tfrec_dir/ f"val_shard_{input_shard_num}-8611"
+        test_tfrec_fp = test_tfrec_dir / f"test_shard_{input_shard_num}-8611"
 
-    writers = {
-        "train" : tf.io.TFRecordWriter(train_tfrec_fp),
-        "val" : tf.io.TFRecordWriter(val_tfrec_fp),
-        "test" : tf.io.TFRecordWriter(test_tfrec_fp)
-    }
+        train_count = 0
+        val_count = 0
+        test_count = 0
 
-    for str_record in tf.data.TFRecordDataset(input_shard_fp):
-        example = tf.train.Example()
-        example.ParseFromString(str_record)
+        writers = {
+            "train" : tf.io.TFRecordWriter(str(train_tfrec_fp)),
+            "val" : tf.io.TFRecordWriter(str(val_tfrec_fp)),
+            "test" : tf.io.TFRecordWriter(str(test_tfrec_fp))
+        }
 
-        target_id = example.features.feature['target_id'].bytes_list.value[0].decode('utf-8')
+        shard_dataset = tf.data.TFRecordDataset(str(input_shard_fp))
+        for str_record in shard_dataset:
+            example = tf.train.Example()
+            example.ParseFromString(str_record.numpy())
 
-        if target_id in train_targets:
-            split = "train"
-            train_count += 1
-        elif target_id in val_targets:
-            split = "val"
-            val_count += 1
-        elif target_id in test_targets:
-            split = "test"
-            test_count += 1
-        else:
-            raise ValueError(f"Target ID {target_id} not found in any set")
+            target_id = example.features.feature['target_id'].int64_list.value[0]
+            
+            split = ""
+
+            if target_id in train_targets:
+                split = "train"
+                train_count += 1
+            elif target_id in val_targets:
+                split = "val"
+                val_count += 1
+            elif target_id in test_targets:
+                split = "test"
+                test_count += 1
+            else:
+                raise ValueError(f"Target ID {target_id} not found in any set")
+            
+            writers[split].write(str_record.numpy())
+            
         
-        writers[split].write(str_record.numpy())
-    
-    for writer in writers.values():
-        writer.close()
+        for writer in writers.values():
+            writer.close()
 
-    tfrec_tbls = []
+        tfrec_tbls = []
 
-    data_fields = {
-        'uid': 'str',
-        'target_id': 'int',
-        'tce_plnt_num': 'int',
-        'sector_run': 'str',
-        'label': 'str',
-    }
+        data_fields = {
+            'uid': 'str',
+            'label': 'float',
+            'target_id': 'int',
+            'tce_plnt_num': 'int',
+            'numberOfPlanets': 'int',
+            'tce_num_transits': 'int',
+            'tce_num_transits_obs': 'int',
+            'mag_cat': 'int',
+            'mission': 'int',
+            'sector': 'str',
+            'tce_depth': 'float',
+            'tce_period': 'float',
+            'tce_time0bk': 'float',
+            'tce_duration': 'float',
+            'disposition': 'str',
+            'disposition_source' : 'str'
+        }
 
-    if train_count > 0:
-        tfrec_tbls.append(create_table_with_tfrecord_examples(train_tfrec_fp, data_fields))
-    
-    if val_count > 0:
-        tfrec_tbls.append(create_table_with_tfrecord_examples(val_tfrec_fp, data_fields))
-    
-    if test_count > 0:
-        tfrec_tbls.append(create_table_with_tfrecord_examples(test_tfrec_fp, data_fields))
+        print(f"examples in train_set: {train_count}")
+        print(f"examples in val_set: {val_count}")
+        print(f"examples in test_set: {test_count}")
 
-    tfrec_tbl = pd.concat(tfrec_tbls, axis=0)
+        if train_count > 0:
+            tfrec_tbls.append(create_table_with_tfrecord_examples(train_tfrec_fp, data_fields))
+        
+        if val_count > 0:
+            tfrec_tbls.append(create_table_with_tfrecord_examples(val_tfrec_fp, data_fields))
 
-    tfrec_tbl.to_csv(output_dir / f'shards_tbl_{input_shard_num}-8611.csv', index=False)
+        if test_count > 0:
+            tfrec_tbls.append(create_table_with_tfrecord_examples(test_tfrec_fp, data_fields))
 
-    print(f"Processed shard: {input_shard_fp}")
+        tfrec_tbl = pd.concat(tfrec_tbls, axis=0)
 
-    return f"Completed: {input_shard_fp}"
+        aux_tbl_path = output_dir / 'aux_tbls'
+        aux_tbl_path.mkdir(parents=True, exist_ok=True)
+
+        tfrec_tbl.to_csv(aux_tbl_path / f'shards_tbl_{input_shard_num}-8611.csv', index=False)
+
+        print(f"COMPLETED processing tfrecord {str(input_shard_fp).split('/')[-1]} successfully")
+
+        return f"COMPLETED processing tfrecord {str(input_shard_fp).split('/')[-1]} successfully with {train_count} train ex., {val_count} val ex., and {test_count} test ex."
+
+    except Exception as e:
+        print(f"ERROR while processing tfrecord {str(input_shard_fp).split('/')[-1]}: {e}")
+        return f"ERROR while processing tfrecord {str(input_shard_fp).split('/')[-1]}: {e}"
 
 
 if __name__ == "__main__":
     # load merged aux tbl
-    aux_tbl = pd.read_csv("merged_data_tbl_chunks_0001-8611.csv")
+    aux_tbl = pd.read_csv("/nobackup/jochoa4/work_dir/data/datasets/TESS_exoplanet_dataset_11-12-2024/merged_tfrecords/merged_data_tbl_chunks_0001-8611.csv")
 
     # src directory for raw tfrecord shards
-    src_tfrec_dir = Path("/nobackup/jochoa4/work_dir/data/datasets/TESS_exoplanet_dataset_11-25-2024")
-
-    # src fp for auxillary table
-    src_aux_tbl_fp = Path("/nobackup/jochoa4/work_dir/data/datasets/TESS_exoplanet_dataset_11-25-2024/data_tbl.csv")
+    src_tfrec_dir = Path("/nobackup/jochoa4/work_dir/data/datasets/TESS_exoplanet_dataset_11-12-2024/tfrecords")
 
     # destination directory for tfrecord shard splits
-    dest_tfrec_dir = Path("/nobackup/jochoa4/work_dir/data/datasets/TESS_exoplanet_dataset_11-25-2024_split/tfrecords")
+    dest_tfrec_dir = Path("/nobackup/jochoa4/work_dir/data/datasets/TESS_exoplanet_dataset_11-12-2024_split/tfrecords")
+    dest_tfrec_dir.mkdir(parents=True, exist_ok=True)
 
     # destination directory for logging
-    dest_log_dir = Path("/nobackup/jochoa4/work_dir/data/logging") #TODO: revisit and update dest_log_dir
+    log_dir = Path("/nobackup/jochoa4/work_dir/data/logging/split_dataset_logs") #TODO: revisit and update dest_log_dir
+    log_dir.mkdir(parents=True, exist_ok=True)
+
+    # log dataset split
+    logger = logging.getLogger(f"split_dataset_logger")
+    logger.setLevel(logging.INFO)
+
+    log_path = Path(log_dir) / f"split_dataset.log"
+    file_handler = logging.FileHandler(log_path)
+    logger_formatter = logging.Formatter('%(asctime)s - %(levelname)s- %(message)s')
+    file_handler.setFormatter(logger_formatter)
+    logger.addHandler(file_handler)
 
     # set random seed
     np.random.seed(42) 
@@ -119,6 +165,9 @@ if __name__ == "__main__":
     # number of unique targets
     N = len(shuffled_targets)
 
+    print(f"Processing: {N} unique targets")
+    logger.info(f"Processing: {N} unique targets")
+
     # compute exact sizes for each split
     num_train = int(N * 0.8)
     num_val = int(N * 0.1)
@@ -133,36 +182,25 @@ if __name__ == "__main__":
     print(f'Using {len(val_targets)} targets in validation set')
     print(f'Using {len(test_targets)} targets in test set')
 
+    logger.info(f'Using {len(train_targets)} targets in training set')
+    logger.info(f'Using {len(val_targets)} targets in validation set')
+    logger.info(f'Using {len(test_targets)} targets in test set')
+
     # Save the dataset targets to a npy file
     dataset_targets = {}
     dataset_targets['train_set'] = train_targets
     dataset_targets['val_set'] = val_targets
     dataset_targets['test_set'] = test_targets
 
-    np.save('dataset_targets.npy', dataset_targets)
+    # save targets to numpy for later validation
+    target_npy_path = Path('/nobackupp27/jochoa4/work_dir/data/stats/')
+    target_npy_path.mkdir(parents=True, exist_ok=True)
+    target_npy_path = target_npy_path / "split_dataset_targets.npy"
 
-    # # define logging??
+    np.save(target_npy_path, dataset_targets)
 
-    # job_log_dir = log_dir / 'job_logs'
-    # job_log_dir.mkdir(parents=True, exist_ok=True)
-
-
-    #     # log chunk validation
-    # logger = logging.getLogger(f"chunk_validation_logger")
-    # logger.setLevel(logging.INFO)
-
-    # log_path = Path(log_dir) / f"chunk_validation.log"
-    # file_handler = logging.FileHandler(log_path)
-    # logger_formatter = logging.Formatter('%(asctime)s - %(levelname)s- %(message)s')
-    # file_handler.setFormatter(logger_formatter)
-    # logger.addHandler(file_handler)
-
-    # logger.info(f'Processing a total of {len(chunked_jobs)} chunks of size {job_chunk_size}')
-    # print(f'Processing a total of {len(chunked_jobs)} chunks of size {job_chunk_size}')
-
-
-    # logger.info(f'Skipping processing for {sum(processed_chunk_mask)} chunks that have already been processed.')
-
+    logger.info(f'Saving targets to: {target_npy_path}')
+    print(f'Saving targets to: {target_npy_path}')
 
 
     # define partial func to predefine default values for directories and target sets
@@ -174,17 +212,25 @@ if __name__ == "__main__":
                                 val_targets=val_targets,
                                 test_targets=test_targets
                                 )
+    start = time.time()
+    # using N-2 = 126 as tested working value
+    pool = multiprocessing.Pool(processes=126)
 
-    # using N-1 = 127
-    pool = multiprocessing.Pool(processes=127)
+    jobs = [shard_num for shard_num in range(1, 8611 + 1)] # chunk 1 to chunk 8611
 
-    jobs = [shard_num for shard_num in range(1, 8612)] # chunk 1 to chunk 8611
+    logger.info(f'Beginning processing {len(jobs)} chunks from range {jobs[0]} to {jobs[-1]}')
+    print(f'Beginning processing {len(jobs)} chunks from range {jobs[0]} to {jobs[-1]}')
 
+    results = []
     for shard_num in jobs:
-        pool.apply_async(partial_func, args=shard_num)
-
+        result = pool.apply_async(partial_func, args=[shard_num])
+        results.append(result)
+    
+    for result in results:
+        logger.info(result.get())
     pool.close()
     pool.join()
     
-    print('Finished processing train val test split')
+    end = time.time()
+    print(f'Finished processing train val test split using 128 shards in {end - start} seconds')
 
