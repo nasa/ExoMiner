db6418516a10fb95bd8c7f9f0f1e4893eb47af3d
Josue Ochoa <josue.j.ochoa@nasa.gov>
2025-02-17 16:06:55 -0800

    Temp commit: Currently functioning for local testing with output visualization

--- transit_detection/norm_pipeline/norm_tfrec_dataset_split.py
+++ transit_detection/norm_pipeline/norm_tfrec_dataset_split.py
@@ -59,7 +59,12 @@ def normalize_img_feature(img_feature, img_set_med, img_set_std):
     return norm_img_feature
 
 def normalize_and_write_shard(src_tfrec_fp, dest_tfrec_fp, dest_norm_stats_dir):
+    if not src_tfrec_fp.exists():
+        print("FILEPATH DOES NOT EXIST")
+        print(src_tfrec_fp.name)
+        return
 
+    print(f"\nSRC: {src_tfrec_fp}\nDEST: {dest_tfrec_fp}\nSTATS: {dest_norm_stats_dir}\n")
     # dest_tfrec_fp = dest_tfrec_dir / src_tfrec_fp.name
     try:
         with tf.io.TFRecordWriter(path=str(dest_tfrec_fp)) as writer:
@@ -67,15 +72,23 @@ def normalize_and_write_shard(src_tfrec_fp, dest_tfrec_fp, dest_norm_stats_dir):
             # Load source shard as tfrec dataset
             src_tfrecord_dataset = tf.data.TFRecordDataset(src_tfrec_fp)
 
+            example_count = 0
             for string_record in src_tfrecord_dataset.as_numpy_iterator():
+                example_count += 1
 
                 example = tf.train.Example()
 
                 example.ParseFromString(string_record)
 
                 # normalize flux window
-                example_flux = example.features.feature['flux'].float_list.value
+                example_flux = np.array(example.features.feature['flux'].float_list.value)
+                
+                
                 example_norm_flux = normalize_flux(example_flux)
+                if example_count == 1:
+                    print(f"\nFLUX: {example_flux}\nFLUX_SHAPE: {example_flux.shape}\n")
+                    print(f"\nNORM_FLUX: {example_norm_flux}\nNORM_FLUX_SHAPE: {example_norm_flux}\n")
+                
 
                 example_util.set_float_feature(ex=example, name='flux_norm', value=example_norm_flux, allow_overwrite=False)
 
@@ -84,12 +97,18 @@ def normalize_and_write_shard(src_tfrec_fp, dest_tfrec_fp, dest_norm_stats_dir):
 
                 for img_feature in ['diff_img', 'oot_img', 'snr_img']:
                     # get median/std etc from somewhere for training / val set
-                    norm_stats = np.load(dest_norm_stats_dir / f"{img_feature}_stats.npy")
+                    norm_stats = np.load(dest_norm_stats_dir / f"train_set_{img_feature}_stats.npy", allow_pickle=True).item()
+                    
                     img_feature_set_med, img_feature_set_std = norm_stats['median'], norm_stats['std']
 
                     example_img_feature = tf.reshape(tf.io.parse_tensor(example.features.feature[img_feature].bytes_list.value[0], tf.float32),
                                                         img_dims).numpy()
+                    
                     example_norm_img_feature = normalize_img_feature(example_img_feature, img_feature_set_med, img_feature_set_std)
+                    if example_count == 1:
+                        print(f"TRAIN SET MED: {img_feature_set_med} and STD: {img_feature_set_std}")
+                        print(f"{img_feature}_FEATURE: {example_img_feature}")
+                        print(f"NORM_{img_feature}_FEATURE: {example_norm_img_feature}")
 
                     example_util.set_tensor_feature(ex=example, name=f"{img_feature}_stdnorm", value=example_norm_img_feature,
                                                 allow_overwrite=False)
@@ -105,14 +124,17 @@ def normalize_and_write_shard(src_tfrec_fp, dest_tfrec_fp, dest_norm_stats_dir):
 if __name__ == "__main__":
 
     # Define split name when processing multiple split directories
-    SPLIT_NAME = "train"
+    SPLIT_NAME = "test" #TODO: switch back to train split
 
     # Define logging directory
-    log_dir = None
+    log_dir = Path("/Users/jochoa4/Desktop/test_runs/norm_test_run/logging")
+    log_dir.mkdir(parents=True, exist_ok=True)
 
-    # Define source dataset directory & dest dataset directory with same structure + norm suffix
-    src_dataset_dir = Path('/nobackupp27/jochoa4/work_dir/data/datasets/TESS_exoplanet_dataset_11-12-2024_split')
-    dest_dataset_dir = src_dataset_dir.parent / str(src_dataset_dir.name) + '_norm'
+    # # Define source dataset directory & dest dataset directory with same structure + norm suffix
+    # src_dataset_dir = Path('/nobackupp27/jochoa4/work_dir/data/datasets/TESS_exoplanet_dataset_11-12-2024_split')
+    # TEST:
+    src_dataset_dir = Path('/Users/jochoa4/Desktop/test_runs/norm_test_run/dataset_1_split')
+    dest_dataset_dir = src_dataset_dir.parent / (str(src_dataset_dir.name) + '_norm')
 
     # Define source/destination tfrec directory based on src dataset + split_name specification
     src_tfrec_dir = src_dataset_dir / 'tfrecords' / SPLIT_NAME
@@ -120,11 +142,13 @@ if __name__ == "__main__":
     dest_tfrec_dir.mkdir(parents=True, exist_ok=True)
 
     # Define destination norm stats directory for the split
-    dest_norm_stats_dir = Path('/nobackup/jochoa4/work_dir/data/stats') / dest_dataset_dir.name / SPLIT_NAME
+    # dest_norm_stats_dir = Path('/nobackup/jochoa4/work_dir/data/stats') / dest_dataset_dir.name / SPLIT_NAME
+    #TEST: 
+    dest_norm_stats_dir = Path('/Users/jochoa4/Desktop/test_runs/norm_test_run/stats') / dest_dataset_dir.name / SPLIT_NAME
     dest_norm_stats_dir.mkdir(parents=True, exist_ok=True)
 
     # Define pool for multiprocessing
-    pool = multiprocessing.Pool(processes=126)
+    pool = multiprocessing.Pool(processes=1) #TODO: move up processes
 
     # Setup logger for overseeing parallelized processes
     logger = logging.getLogger(f"norm_logger_{SPLIT_NAME}")
@@ -138,7 +162,7 @@ if __name__ == "__main__":
 
     # Define shard format
     SHARD_PREFIX = SPLIT_NAME + '_shard_'
-    NUM_SHARDS = 8611
+    NUM_SHARDS = 16
 
     # Create list of shard_nums to process from 1 to NUM_SHARDS bound
     shards_to_process = [shard_num for shard_num in range(1, NUM_SHARDS + 1)]
@@ -148,13 +172,14 @@ if __name__ == "__main__":
     # Process shards in parallel using pool
     for shard_num in shards_to_process:
         logger.info(f'Processing chunk {shard_num}.')
+        print((f'Processing chunk {shard_num}.'))
 
         # define title in {SHARD_PREFIX}_{SHARD_NUM}-{NUM_SHARDS} format ex) test_shard_XXXX-8611 format
         SHARD_TITLE = SHARD_PREFIX + str(shard_num).zfill(4) + '-' + str(NUM_SHARDS).zfill(4)  
 
         # define src tfrec to process and dest tfrec to write to
         src_tfrec_fp = src_tfrec_dir / SHARD_TITLE
-        dest_tfrec_fp = dest_tfrec_dir / 'norm_' + SHARD_TITLE
+        dest_tfrec_fp = dest_tfrec_dir / ('norm_' + SHARD_TITLE)
 
         result = pool.apply_async(normalize_and_write_shard, args=(src_tfrec_fp, dest_tfrec_fp, dest_norm_stats_dir))
         results.append(result.get())
